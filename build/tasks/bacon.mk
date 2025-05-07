b6b2edfe025ff51fa02d9dd4523b482d2945da71
HinohArata <mmgcntk@gmail.com>
2025-05-07 12:25:51 +0000

    version: Bump to v13.3 with May Security Patch
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -1,17 +1,19 @@
 #
 # Copyright (C) 2018-2023 ArrowOS
+#           (C) 2025 ArrowOS Extended
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 ARROW_TARGET_PACKAGE := $(PRODUCT_OUT)/$(ARROW_VERSION).zip
+TARGET_PACKAGE_RELATIVE := $(subst $(shell pwd)/,,$(ARROW_TARGET_PACKAGE))
 
 .PHONY: bacon
 bacon: otapackage
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(ARROW_TARGET_PACKAGE)
 	$(hide) $(MD5SUM) $(ARROW_TARGET_PACKAGE) > $(ARROW_TARGET_PACKAGE).md5sum
 	$(hide) ./vendor/arrow/build/tools/generate_ota_json.sh $(TARGET_DEVICE) $(PRODUCT_OUT) $(ARROW_VERSION).zip $(ARROW_MAINTAINER) $(ARROW_MOD_VERSION)
-	echo -e "========================================="
+	echo -e "========================================================================================================"
 	echo -e ""
 	echo -e "                 \033[1;34m''\033[0m                  "
 	echo -e "           \`\`\`\`\`\033[1;34m.oy.\033[0m\`\`\`\`\`\`\`          "
@@ -32,6 +34,8 @@ bacon: otapackage
 	echo -e ""
 	echo -e "                ArrowOS"
 	echo -e ""
-	echo -e "========================================="
-	echo -e ""
-	@echo "**Package completed**: $(ARROW_TARGET_PACKAGE)"
+	echo -e "========================================================================================================"
+	echo -e "\033[94m=>\033[0m Zip File : \033[93m$(TARGET_PACKAGE_RELATIVE)\033[0m"
+	echo -e "\033[91m=>\033[0m MD5      : \033[94m$$(md5sum $(ARROW_TARGET_PACKAGE) | awk '{print $$1}')\033[0m"
+	echo -e "\033[93m=>\033[0m Size     : \033[91m$$(du -h $(ARROW_TARGET_PACKAGE) | awk '{print $$1}')\033[0m"
+	echo -e "========================================================================================================"
